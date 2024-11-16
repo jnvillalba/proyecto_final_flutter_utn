cdb992b272258247e87922d7160d754d67f2e9fb
jnvillalba <joaquinvillalba8@gmail.com>
2024-11-16 16:12:10 -0300

    add firebase login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:proyecto_final_facil/components/custom_btn.dart';
 import 'package:proyecto_final_facil/components/custom_textfield.dart';
 
 void main() {
-  runApp(LoginPage());
+  runApp(const LoginPage());
 }
 
 class LoginPage extends StatefulWidget {
@@ -12,49 +13,49 @@ class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _LoginPageState();
 }
+
 class _LoginPageState extends State<LoginPage> {
   final userNameController = TextEditingController();
   final passwordController = TextEditingController();
 
   void _signIn() async {
-  print('User: ${userNameController.text}');
-  print('Password: ${passwordController.text}');
-  BuildContext dialogContext = context;
-  showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return const AlertDialog(
-          title: Text('Logging in...'),
-          content: LinearProgressIndicator(),
-        );
-      });
-  try {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: userNameController.text,
-      password: passwordController.text,
-    );
-    ifMounted(dialogContext, mounted)
-
-  } on FirebaseAuthException catch (e) {
-    ifMounted(dialogContext, mounted)
-    print('Error initializing Firebase: $e');
-    Navigator.of(dialogContext).pop();
-  } catch (e) {
-    ifMounted(dialogContext, mounted)
-    print('Error catch (: $e');
-    Navigator.of(dialogContext).pop();
+    print('User: ${userNameController.text}');
+    print('Password: ${passwordController.text}');
+    BuildContext dialogContext = context;
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return const AlertDialog(
+            title: Text('Logging in...'),
+            content: LinearProgressIndicator(),
+          );
+        });
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: userNameController.text,
+        password: passwordController.text,
+      );
+      ifMounted(dialogContext, mounted);
+    } on FirebaseAuthException catch (e) {
+      ifMounted(dialogContext, mounted);
+      print('Error initializing Firebase: $e');
+      Navigator.of(dialogContext).pop();
+    } catch (e) {
+      ifMounted(dialogContext, mounted);
+      print('Error catch (: $e');
+      Navigator.of(dialogContext).pop();
+    }
   }
-}
 
-  // void ifMounted((dialogContext, mounted) {
-  // if (mounted) {
-  // Navigator.of(dialogContext).pop();
-  // Navigator.of(context).pushReplacementNamed('/home');
-  // } else {
-  // print('Error initializing Firebase: not mounted', mounted);
-  // }
-  // });
+  void ifMounted(dialogContext, mounted) {
+    if (mounted) {
+      Navigator.of(dialogContext).pop();
+      Navigator.of(context).pushReplacementNamed('/home');
+    } else {
+      print('Error initializing Firebase: not mounted $mounted');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
