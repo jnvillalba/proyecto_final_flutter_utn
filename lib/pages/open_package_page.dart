6d435ab590637cbf4574ad63cc40fd9d4870c739
jnvillalba <joaquinvillalba8@gmail.com>
2024-11-24 18:05:46 -0300

    open pack persistence wip

--- lib/pages/open_package_page.dart
+++ lib/pages/open_package_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:proyecto_final_facil/components/empty_sticker.dart';
 import 'package:proyecto_final_facil/components/open_package/package.dart';
 import 'package:proyecto_final_facil/components/open_package/shake_widget.dart';
-import 'package:proyecto_final_facil/data.dart';
+import 'package:proyecto_final_facil/models/player.dart';
+import 'package:proyecto_final_facil/services/player_service.dart';
 import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 class OpenPackagePage extends StatefulWidget {
@@ -15,16 +16,7 @@ class OpenPackagePage extends StatefulWidget {
 class OpenPackageState extends State<OpenPackagePage> {
   final shakeKey = GlobalKey<ShakeWidgetState>();
   bool showStikcers = false;
-
   final List<String> titles = ['', '', '', '', ''];
-
-  final List<Widget> images = [
-    EmptySticker(player: playersBoca()[0]),
-    EmptySticker(player: playersBoca()[1]),
-    EmptySticker(player: playersBoca()[2]),
-    EmptySticker(player: playersBoca()[3]),
-    EmptySticker(player: playersBoca()[4])
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,56 +29,89 @@ class OpenPackageState extends State<OpenPackagePage> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisSize: MainAxisSize.min,
               children: [
-                showStikcers
-                    ? Expanded(
-                        child: VerticalCardPager(
-                          titles: titles,
-                          images: images,
-                          onSelectedItem: (index) {},
-                          initialPage: 2,
-                          align: ALIGN.CENTER,
-                          physics: const ClampingScrollPhysics(),
-                        ),
-                      )
-                    : ShakeWidget(
-                        key: shakeKey,
-                        shakeCount: 6,
-                        shakeOffset: 10,
-                        shakeDuration: const Duration(milliseconds: 600),
-                        onShakeComplete: () {
-                          setState(() {
-                            showStikcers = true;
-                          });
-                        },
-                        child: GestureDetector(
-                          onTap: () {
-                            shakeKey.currentState?.shake();
-                          },
-                          child: Visibility(
-                            visible: !showStikcers,
-                            child: const Column(
-                              children: [
-                                Package(),
-                                SizedBox(height: 20),
-                                Text(
-                                  'Toca para abrir',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Colors.white70,
+                FutureBuilder<List<Player>>(
+                  future: openPack(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Text('No hay jugadores disponibles');
+                    } else {
+                      final players = snapshot.data!;
+                      final images = players
+                          .map((player) => EmptySticker(player: player))
+                          .toList();
+
+                      return showStikcers
+                          ? Expanded(
+                              child: VerticalCardPager(
+                                titles: titles,
+                                images: images,
+                                onSelectedItem: (index) {
+                                  _deletePlayer(players[index]);
+                                },
+                                initialPage: 2,
+                                align: ALIGN.CENTER,
+                                physics: const ClampingScrollPhysics(),
+                              ),
+                            )
+                          : ShakeWidget(
+                              key: shakeKey,
+                              shakeCount: 6,
+                              shakeOffset: 10,
+                              shakeDuration: const Duration(milliseconds: 600),
+                              onShakeComplete: () {
+                                setState(() {
+                                  showStikcers = true;
+                                });
+                              },
+                              child: GestureDetector(
+                                onTap: () {
+                                  shakeKey.currentState?.shake();
+                                },
+                                child: Visibility(
+                                  visible: !showStikcers,
+                                  child: const Column(
+                                    children: [
+                                      Package(),
+                                      SizedBox(height: 20),
+                                      Text(
+                                        'Toca para abrir',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                          color: Colors.white70,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ],
                                   ),
-                                  textAlign: TextAlign.center,
                                 ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
+                              ),
+                            );
+                    }
+                  },
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _deletePlayer(Player player) async {
+    try {
+      //descartar jugador
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error al guardar jugador: $e'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
   }
 }
